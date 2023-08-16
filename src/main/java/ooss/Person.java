f2de52b242d5ed4c8d5219ad45cf74db0d22c425
JESSRIEL JAMILLA <jamillajessriel17@gmail.com>
2023-08-16 17:38:21 +0800

    feat: Add implementation for introduce method of person and will return an introduction

--- src/main/java/ooss/Person.java
+++ src/main/java/ooss/Person.java
@@ -1,5 +1,34 @@
 package ooss;
 
+import java.util.Objects;
+
 public class Person {
 
+
+    private final int id;
+    private final String name;
+    private final int age;
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return id == person.id && age == person.age;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, age);
+    }
+
+    public String introduce(){
+        return String.format("My name is %s. I am %s years old.", name, age);
+    }
 }
--- src/main/java/ooss/Student.java
+++ src/main/java/ooss/Student.java
@@ -2,4 +2,9 @@ package ooss;
 
 public class Student extends Person {
 
+
+    public Student(int id, String name, int age) {
+        super(id, name, age);
+    }
+
 }
