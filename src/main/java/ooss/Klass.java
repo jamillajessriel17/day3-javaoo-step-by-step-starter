db41bffef34745ffe02d3b269e3eef8ca433c549
JESSRIEL JAMILLA <jamillajessriel17@gmail.com>
2023-08-16 19:09:29 +0800

    feat: Override implementation of equals and hashCode for Klass

--- src/main/java/ooss/Klass.java
+++ src/main/java/ooss/Klass.java
@@ -1,5 +1,25 @@
 package ooss;
 
+import java.util.Objects;
+
 public class Klass {
 
+    private final int id;
+
+    public Klass(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Klass klass = (Klass) o;
+        return id == klass.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
